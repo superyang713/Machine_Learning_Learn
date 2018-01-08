6c4e0fd5c4461be83c4d8014f3e0fa0aa45b34e6
superyang713 <yangdai713@aim.com>
2018-01-07 21:52:43 -0500

    update

---
+++ tensorflow_learn/normal_equation.py
@@ -0,0 +1,15 @@
+import numpy as np
+from sklearn.datasets import fetch_california_housing
+import tensorflow as tf
+
+housing = fetch_california_housing()
+m, n = housing.data.shape
+housing_data_plus_bias = np.c_[np.ones((m, 1)), housing.data]
+
+X = tf.constant(housing_data_plus_bias, dtype=tf.float32, name="X")
+y = tf.constant(housing.target.reshape(-1, 1), dtype=tf.float32, name="y")
+XT = tf.transpose(X)
+theta = tf.matmul(tf.matmul(tf.matrix_inverse(tf.matmul(XT, X)), XT), y)
+
+with tf.Session() as sess:
+    theta_value = theta.eval()
